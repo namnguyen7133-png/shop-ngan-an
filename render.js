0386aebb8d0a6b43728187eb24c6398376ea2a8c
namnguyen7133-png <namnguyen7133@gmail.com>
2026-02-19 22:20:22 +0700

    Update render.js

--- render.js
+++ render.js
@@ -1,12 +1,12 @@
 const box = document.getElementById("list");
 
-if (!ON) {
-  box.innerHTML = "<p>⛔ Trang đang tắt</p>";
+if (typeof PRODUCTS === "undefined" || PRODUCTS.length === 0) {
+  box.innerHTML = "<p>❌ Không có dữ liệu sản phẩm</p>";
 } else {
   PRODUCTS.forEach(p => {
     box.innerHTML += `
       <div class="item">
-        <img src="${p.img}">
+        <img src="${p.img}" alt="${p.name}">
         <h3>${p.name}</h3>
         <p>${p.price}</p>
         <a class="btn" href="${p.link}" target="_blank">Mua ngay</a>
